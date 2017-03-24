147c2e1cb739bdb0a23eb21847257561d1219d76
regendare <philliple1337@gmail.com>
2017-03-24 14:38:10 -0700

    idk what changed

--- spyfall/player.py
+++ spyfall/player.py
@@ -1,5 +1,5 @@
 # object to keep track of players in the game
 class Player:
-    def __init__ (self, discord_user_name):
+    def __init__(self, discord_user_name):
         self.name = discord_user_name
         self.role = None
