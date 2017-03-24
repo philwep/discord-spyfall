5095541ddec95baef7213ef86bfceb19a3009cfe
Fabio Colacio <fizzcol@gmail.com>
2017-03-24 10:45:58 -0700

    added role distribution functionality to game class
    created an assign roles method, which takes a list of the
    roles as a parameter
    
    Would like to parse the csv/json files internally in the game
    class eventually, but currently working with what we have

--- spyfall/game.py
+++ spyfall/game.py
@@ -1,5 +1,8 @@
+from player import Player
+from random import randint
+
 # object to keep track of game state
-class game:
+class Game:
     def __init__ (self):
 
         # list of player objects
@@ -12,3 +15,25 @@ class game:
         for player in self.players:
             if (player.role == "spy"):
                 print("The spy was %s!" % player.discord_user_name)
+
+    def join_player (self, discord_user_name):
+        player = Player(discord_user_name)
+        self.players.append(player)
+
+    def _assign_roles (self, roles):
+        spy = randint(0,len(self.players)-1)
+        self.players[spy].role = "spy"
+
+        cur_role = 0
+        for player in self.players:
+            if (player.role != "spy"):
+                player.role = roles[cur_role]
+
+            if (cur_role + 1 >= len(roles)):
+                cur_role = 0
+            else:
+                cur_role += 1
+
+
+    def start_game (self):
+        self._assign_roles()
