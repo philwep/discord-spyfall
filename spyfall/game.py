fe4ef7bca648d1935cfc77e9b2d50124d50d23ef
regendare <philliple1337@gmail.com>
2017-03-24 14:37:21 -0700

    Added json parser into game.py

--- spyfall/game.py
+++ spyfall/game.py
@@ -1,9 +1,11 @@
 from player import Player
 from random import randint
+import os
+import json
 
 # object to keep track of game state
 class Game:
-    def __init__ (self):
+    def __init__(self):
 
         # list of player objects
         self.players = []
@@ -11,16 +13,24 @@ class Game:
         # keeps track of the current location
         self.location = None
 
-    def end_game (self):
+        # locations and roles
+        self.locations_file = []
+
+    def end_game(self):
         for player in self.players:
             if (player.role == "spy"):
                 print("The spy was %s!" % player.discord_user_name)
 
-    def join_player (self, discord_user_name):
+    def join_player(self, discord_user_name):
         player = Player(discord_user_name)
         self.players.append(player)
 
-    def _assign_roles (self, roles):
+    def leave_player(self, discord_user_name):
+        player = Player(discord_user_name)
+        if player in self.players:
+            self.players.remove(player)
+
+    def assign_roles(self, roles):
         spy = randint(0,len(self.players)-1)
         self.players[spy].role = "spy"
 
@@ -34,6 +44,12 @@ class Game:
             else:
                 cur_role += 1
 
+    def get_roles(self, file_name):
+        if os.path.exists(file_name):
+            with open(file_name, 'r') as locations_file:
+                self.locations_file = json.load(locations_file)
+        else:
+            print("The file %s does not exist or cannot be opened" % file_name)
 
-    def start_game (self):
-        self._assign_roles()
+    def start_game(self):
+        self.assign_roles()
---
+++ spyfall/test.py
@@ -0,0 +1,9 @@
+import game
+
+test = game.Game()
+
+test.get_roles('spyfall_locations.json')
+
+why = test.locations_file
+
+print(why)
