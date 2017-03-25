f0635c9f03aadb4accc82c7c6806a7215192d051
regendare <philliple1337@gmail.com>
2017-03-25 10:33:12 -0700

    removed hello again

--- spyfall/debug.py
+++ spyfall/debug.py
@@ -9,7 +9,6 @@ game.start_game()
 print("Location: %s" % game._game_data['locations'][game.location]['Location'])
 print("Roles: %s" % game._game_data['locations'][game.location]['Roles'])
 
-
 print("Players:")
 for player in game.players:
     print("\tName:%s Role:%s" % (player.name, player.role))
