81327bb987cc638177252beb037eaf4bfc38bc3c
regendare <philliple1337@gmail.com>
2017-03-24 20:32:09 -0700

    Overhaul'd spyfall to discord interface

--- spyfall/discord_spyfall.py
+++ spyfall/discord_spyfall.py
@@ -1,102 +1,45 @@
-""" March 09, 2017
-@author: Phillip Le
-@author: Fabio Colacio
-"""
-
+import game
+import player
 import discord
 import asyncio
-import random
-import os
 import json
+import os
 
-from player import Player
-from game import Game
+if os.path.exists('config.json'):
+    with open('config.json','r') as configs:
+        configs = json.load(configs)
 
-# open the config file
-with open('config.json', 'r') as configuration_file:
-    configuration_file = json.load(configuration_file)
+bot_token = configs['discord']['token']
+bot_trigger = configs['discord']['trigger']
+bot_description = "Spyfall"
 
-discord_token = configuration_file['discord']['token']
-debug = configuration_file['other']['debug']
+game = game.Game()
+bot = discord.Client()
 
-with open('spyfall_locations.json', 'r') as locations_file:
-    locations_file = json.load(locations_file)
+@bot.event
+async def on_ready():
+    print("Bot User-Name: %s" % bot.user)
+    print("Bot User-ID: %s" % bot.user.id)
+    print("--------------------")
 
-print(locations_file)
-
-
-def get_data(spyfall_filename):
-    '''
-    Parses a .csv file
-
-    Args:
-        spyfall_filename: .csv file to be opened and parsed into a dictionary that has keys as location names and values as a list that contains all the roles
-
-    Returns:
-        A list that has a dictionary pointing to a list
-
-        Example Output:
-
-            [location_name:[role_1, role_2, role_3]]
-    '''
-    if os.path.exists(spyfall_filename):
-        with open(spyfall_filename, 'r') as data_file:
-            lines = data_file.readlines()  # read all lines from the file into a list
-            roles = {}
-            for i in range(len(lines)):
-                parts = lines[i].split(',')
-                roles[parts[0]] = [point.strip('\n') for point in parts[1:]]
-            spyfall_data = roles
-            return spyfall_data
-    else:
-        print("The file %s does not exist or cannot be opened" % spyfall_filename)
+@bot.event
+async def on_message(message):
+    message_delete = message
+    message_author = message.author
+    message_author_id = message.author.id
+    message_channel = message.channel
+    message_content = message.content.lower()
 
 
-def get_current_roles(spyfall_data, current_location):
-    '''
-    Get a list of roles for a location
+    if message_content.startswith(bot_trigger + 'join'):
+        game.join_player(message_author_id)
+        await bot.send_message(message.channel, "<@%s> has joined the game." % message_author_id)
 
-    Args:
-        spyfall_data: list object containing all necessary information for locations and roles
-        current_location: variable object containg the current location of the game
+        print(game.players)
+    if message_content.startswith(bot_trigger + 'leave'):
+        game.leave_player(message_author_id)
+        await bot.send_message(message.channel, "<@%s> has left the game." % message_author_id)
 
-    Returns:
-        using the current_location and spyfall_data objects it returns the roles cooresponding to that location, and shuffles the list for randomization
+        print(game.players)
 
-        Example Output:
-
-            [role_2, role_1, role_3, role_4, role_6, role_5]
-    '''
-    current_roles = []
-    for role in spyfall_data[current_location]:
-        current_roles.append(role)
-
-    for shuffle_times in range(0,random.randint(0,10)):
-        random.shuffle(current_roles)
-
-    return current_roles
-
-def main():
-    '''
-    Default run script
-
-    '''
-    spyfall_data = get_data('spyfall_data.csv')
-
-    locations = []
-    for location in spyfall_data:
-        locations.append(location)
-
-    players = ['Player1', 'Player2', 'Player3', 'Player4', 'Player5', 'Player6']
-
-    current_location = locations[random.randint(0,len(locations) - 1)]
-
-    current_roles = get_current_roles(spyfall_data, current_location)
-
-    print(locations)
-    print()
-    print(current_location)
-    print()
-    print(current_roles)
-
-main()
+bot.run(bot_token)
